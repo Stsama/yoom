69e1c4c3312875b41a666615c03936c74bf8385c
st sama <albertezoula98@gmail.com>
2024-12-23 20:28:38 +0000

    addition of webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname), // Map "@" to the project root
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
